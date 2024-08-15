ec5cf98b3e15ee37f3e4db6e7d3b7331e8d49ae5
Everson Mariano <mariano.computacao@gmail.com>
2024-08-14 21:24:39 -0300

    implementando interceptacoes de erros

--- frontend/client/src/app/api/agent.ts
+++ frontend/client/src/app/api/agent.ts
@@ -1,8 +1,29 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { router } from "../router/Router";
 
 axios.defaults.baseURL = 'http://localhost:8081/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.response.use(async response=>{
+    return response
+}, (error: AxiosError)=>{
+    const {status} = error.response as AxiosResponse;
+    switch(status){
+        case 404:
+            console.log('Resource not found - 404 error');
+            router.navigate('/not-found');
+            break;
+        case 500:
+            console.log('Internal server error - 500 error');
+            router.navigate('/server-error');
+            break;
+        default:
+            break;
+    }
+    return Promise.reject(error.message);
+}) 
+
 
 const requests = { 
     get: (url:string) => axios.get(url).then(responseBody),
