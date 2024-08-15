f197ef6ff957421e248625299d3a5a8cdc326898
Everson Mariano <mariano.computacao@gmail.com>
2024-08-15 13:58:51 -0300

    ajuste no backend

--- sports/src/main/java/com/everson/ecommerce/sports/exceptions/CustomExceptionandler.java
+++ sports/src/main/java/com/everson/ecommerce/sports/exceptions/CustomExceptionandler.java
@@ -3,10 +3,12 @@ package com.everson.ecommerce.sports.exceptions;
 import com.everson.ecommerce.sports.model.CustomErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class CustomExceptionandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
