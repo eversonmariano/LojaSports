0f0f121b2fa8f51ba765fc9f919cf738c8fd5f3f
eversonmariano <mariano.computacao@gmail.com>
2024-09-13 21:42:37 -0300

    implementando JWT Helper + Token Expiration

--- sports/src/main/java/com/everson/ecommerce/sports/security/JwtHelper.java
+++ sports/src/main/java/com/everson/ecommerce/sports/security/JwtHelper.java
@@ -2,24 +2,50 @@ package com.everson.ecommerce.sports.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JwtHelper {
 
     public static final long JWT_EXPIRATION_TIME = 5 * 60 * 60;
 
-    private String secret = "ads5dsa456dsa658798weas3wq1";
+    private final String secret = "ads5dsa456dsa658798weas3wq1";
 
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-    private <T> getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = getUsernameFromToken(token);
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return generateToken(claims, userDetails.getUsername());
+    }
+
+    private String generateToken(Map<String, Object> claims, String subject) {
+        //Convert the secret string key int a key object
+        Key hmacKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_TIME * 100))
+                .signWith(hmacKey, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
 
